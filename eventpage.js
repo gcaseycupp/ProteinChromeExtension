48bd97bd3675166f5722922c2da6205060316663
casey <thecupps@gmail.com>
2018-09-13 12:13:08 -0600

    pluralsight creating a chrome extension - protein tracker
    Background page, options, popup.js

---
+++ eventpage.js
@@ -0,0 +1,35 @@
+
+var menuItem= {
+	"id":"AddProtein",
+	"title":"Add Protein",
+	"contexts":['selection']
+}
+
+chrome.contextMenus.create(menuItem);
+
+
+chrome.contextMenus.onClicked.addListener(function contextClicked (clickData){
+
+	if(clickData.menuItemId==='AddProtein' && clickData.selectionText){
+		var intRegex = /^\d+$/;
+		if(intRegex.test(clickData.selectionText)){
+			chrome.storage.sync.get('total', function(items){
+				var newTotal=0;
+				if(items.total){
+					newTotal+=parseInt(items.total);					
+				}
+				newTotal+=parseInt(clickData.selectionText);
+
+				chrome.storage.sync.set({'total':newTotal});
+			});
+		}
+	}
+});
+
+chrome.storage.onChanged.addListener(function (changes) {
+	//alert("newval " + changes.total.newValue.toString());
+	chrome.browserAction.setBadgeText({ "text":changes.total.newValue.toString() }, function(){
+
+	});
+
+});
